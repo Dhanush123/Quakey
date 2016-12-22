0a0c89001ab7327f4c092f80a7772865d0744bbf
Dhanush123 <dhanush.patel@ymail.com>
2016-12-21 22:45:16 -0800

    fix
    dfasdf

--- index.js
+++ index.js
@@ -123,11 +123,12 @@ function USGSCall(lat, long, callback) {
 
 //based on https://gist.github.com/kmaida/6045266
 function convertTimestamp(timestamp) {
+  var offset = (new Date().getTimezoneOffset())/-60;
   var d = new Date(timestamp),	// Convert the passed timestamp to milliseconds
 		yyyy = d.getFullYear(),
 		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
 		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
-		hh = d.getHours(),
+		hh = d.getHours() + offset,
 		h = hh,
 		min = d.getMinutes(),
 		ampm = 'AM',
