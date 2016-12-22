1aef68b4247b86e33a2ad698571cc3cfeba9f560
Dhanush123 <dhanush.patel@ymail.com>
2016-12-21 20:08:48 -0800

    label
    done

--- index.js
+++ index.js
@@ -107,7 +107,8 @@ function USGSCall(lat, long, callback) {
         var location = place.substring(place.indexOf("m") + 1);
         var miles = (place.slice(0, place.indexOf("k")) * 0.621371192).toFixed(2); //convert km to miles and round
         var date = new Date(info.features[0].properties.time);
-        speech = 'The last earthquake in ' + cityName + ' was a ' + mag + ' ' + miles + ' ' + location;
+        var label = miles >= 2 ? ' miles ' : ' mile ';
+        speech = 'The last earthquake in ' + cityName + ' was a ' + mag + ' ' + miles + label + location;
         console.log('USGS speech: ' + speech);
         callback();
       }
