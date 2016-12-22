55a51bf78e8cfa4ceff5d160e91adc4e8de056b9
Dhanush123 <dhanush.patel@ymail.com>
2016-12-21 22:12:30 -0800

    gap remove
    done

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ function USGSCall(lat, long, callback) {
       var place = info.features[0].properties.place;
       var location = place.substring(place.indexOf("m") + 1);
       var miles = (place.slice(0, place.indexOf("k")) * 0.621371192).toFixed(2); //convert km to miles and round
-      var dateTime = (new Date(info.features[0].properties.time)).toLocaleString(). replace(', ', ' at ');
+      var dateTime = (new Date(info.features[0].properties.time)).toLocaleString().replace(', ', ' at ');
       var label = miles >= 2 ? ' miles ' : ' mile ';
       speech = 'The last earthquake in ' + cityName + ' was a ' + mag + miles + label + location + ' on ' + dateTime;
       console.log('USGS speech: ' + speech);
