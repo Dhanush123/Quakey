f872190d74d6b75eed124850f38a8c289294c018
Dhanush123 <dhanush.patel@ymail.com>
2016-12-23 10:11:16 -0800

    fix
    hello

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function getLastCityQuake(requestBody, callback) {
     }
     else {
       if(result.results[0].geometry.location == undefined) {
-        speech = 'I am sorry. I was unable to get the coordinates for the city that you mentioned.'; //put this handling in api.ai later
+        speech = 'I am sorry. I was unable to get the coordinates for the city that you mentioned. Try adding the state name for better results.'; //put this handling in api.ai later
         callback();
       }
       else {
